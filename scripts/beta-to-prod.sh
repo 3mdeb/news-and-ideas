04e9a4898a7a88cd9835bebb5a927c579e5bd0f2
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2019-04-12 14:07:42 +0200

    add build.sh script
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- scripts/beta-to-prod.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sed -e 's/https:\/\/beta.blog.3mdeb.com/https:\/\/blog.3mdeb.com/g' -i ../blog/config.toml
-sed -e 's/https:\/\/beta.3mdeb.com/https:\/\/3mdeb.com/g' -i ../blog/themes/3mdeb/layouts/partials/header.html
---
+++ scripts/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
+  sed -e 's/https:\/\/beta.blog.3mdeb.com/https:\/\/blog.3mdeb.com/g' -i ../blog/config.toml
+  sed -e 's/https:\/\/beta.3mdeb.com/https:\/\/3mdeb.com/g' -i ../blog/themes/3mdeb/layouts/partials/header.html
+fi
+
+cd ../blog
+./hugo
