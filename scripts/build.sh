56dd370a7d1103fc79e921d688805a9f214e1cb2
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2019-04-12 14:20:00 +0200

    build.sh: add echo cmd
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
+  echo "On master branch - setting URLs to production..."
   sed -e 's/https:\/\/beta.blog.3mdeb.com/https:\/\/blog.3mdeb.com/g' -i ../blog/config.toml
   sed -e 's/https:\/\/beta.3mdeb.com/https:\/\/3mdeb.com/g' -i ../blog/themes/3mdeb/layouts/partials/header.html
 fi
