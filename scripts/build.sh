32c13a8b3d8e16b26d2f848019b9d27a672e6b0d
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2019-04-12 14:27:45 +0200

    build.sh: change URL on master
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 echo "Building from branch: ${TRAVIS_BRANCH}"
 
-if [ "${TRAVIS_BRANCH}" = "fix_production_url" ]; then
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
   echo "On master branch - setting URLs to production..."
   sed -e 's/https:\/\/beta.blog.3mdeb.com/https:\/\/blog.3mdeb.com/g' -i ../blog/config.toml
   sed -e 's/https:\/\/beta.3mdeb.com/https:\/\/3mdeb.com/g' -i ../blog/themes/3mdeb/layouts/partials/header.html
