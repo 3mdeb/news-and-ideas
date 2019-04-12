b90e509dc1e4d874992e211a211ae8597252de1e
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2019-04-12 14:24:56 +0200

    build.sh: test
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,13 @@
 
 echo "Building from branch: ${TRAVIS_BRANCH}"
 
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
+if [ "${TRAVIS_BRANCH}" = "fix_production_url" ]; then
   echo "On master branch - setting URLs to production..."
   sed -e 's/https:\/\/beta.blog.3mdeb.com/https:\/\/blog.3mdeb.com/g' -i ../blog/config.toml
   sed -e 's/https:\/\/beta.3mdeb.com/https:\/\/3mdeb.com/g' -i ../blog/themes/3mdeb/layouts/partials/header.html
+
+  echo "Changes:"
+  git diff
 fi
 
 cd ../blog
