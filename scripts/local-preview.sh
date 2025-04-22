96f19fa75b451e37f37471c7680955e75d48001b
Piotr Król <piotr.krol@3mdeb.com>
2025-04-22 22:32:29 +0200

    scripts/local-preview.sh: pass param to container
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- scripts/local-preview.sh
+++ scripts/local-preview.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -v $PWD/blog:/src -p 1313:1313 -u $(id -u) klakegg/hugo:0.92.1 server
+docker run --rm -it -v $PWD/blog:/src -p 1313:1313 -u $(id -u) klakegg/hugo:0.92.1 server -b $@
